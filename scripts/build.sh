fc38552e6bfa981590d3d2877351b67032f8d069
Harrison Kiang <hkiang01@gmail.com>
2020-11-03 21:18:49 -0600

    log the requests

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
+
+registry=registry.harrisonkiang.com/container-registry/admission-webhooks
+
+commit tag based on current git commit hash
 commit_hash=$(git log -n1 --format=format:%H)
-tag=registry.harrisonkiang.com/container-registry/admission-webhooks:$commit_hash
+tag=$registry:$commit_hash
 echo Building and pushing $tag
 docker build -t $tag .
 docker push $tag
+
+# update manifests
+sed -i "s/registry\.harrisonkiang\.com\/container-registry\/admission-webhooks:.*/registry\.harrisonkiang.com\/container-registry\/admission-webhooks:$tag/g" ../kubernetes/deployment.yaml
