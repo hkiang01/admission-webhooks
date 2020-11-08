d3554363a967db4be8f0a3615445b9a4a666c630
Harrison Kiang <hkiang01@gmail.com>
2020-11-07 20:50:29 -0600

    let nginx secure via tls

--- Dockerfile
+++ Dockerfile
@@ -4,11 +4,8 @@ WORKDIR /home
 COPY requirements.txt requirements.txt
 RUN pip install -r requirements.txt 
 
-EXPOSE 8443
+EXPOSE 80
 
 COPY ./app/ ./app/
-
-ENV SSL_KEYFILE=${SSL_KEYFILE}
-ENV SSL_CERTFILE=${SSL_CERTFILE}
 
 CMD ["python", "app/main.py"]
--- app/main.py
+++ app/main.py
@@ -1,5 +1,3 @@
-import os
-
 import uvicorn
 from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
@@ -27,7 +25,5 @@ if __name__ == "__main__":
     uvicorn.run(
         app,
         host="0.0.0.0",
-        port=8443,
-        ssl_keyfile=os.environ["SSL_KEYFILE"],
-        ssl_certfile=os.environ["SSL_CERTFILE"],
+        port=8080,
     )
